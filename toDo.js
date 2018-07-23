4b55a02d17bd25e93b15a23fe125c243bcd14fb4
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-07-23 14:37:24 -0700

    Write out steps.

---
+++ toDo.js
@@ -0,0 +1,9 @@
+/* STEPS
+ * 1. Create HTML content
+ * 2. Add list item when input is submitted
+ * 3. Delete list item when delete button is clicked
+ * 4. Strike through list item when item is clicked
+ * 5. Show/hide input when show/hide button is clicked
+ * 5. Style todo list
+ * 6. Add animation
+ */
