e4b57f452a6def1b6e3eaf560ea210206c84d017
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-07-23 15:17:34 -0700

    Added new to-do item functionality.

--- toDo.js
+++ toDo.js
@@ -7,3 +7,19 @@
  * 5. Style todo list
  * 6. Add animation
  */
+
+// Add keypress event listener to input
+$( "input" ).keypress( function(event) {
+	
+	// Check if enter key is pressed
+	if ( event.which === 13 ) {
+		// Get new to-do item
+		var newToDo = $( "input" ).val();
+
+		// Add new to-do item to list
+		$( "ul" ).append("<li>" + newToDo + "</li>");
+
+		// Clear input
+		$( "input" ).val( "" );
+	}
+});
